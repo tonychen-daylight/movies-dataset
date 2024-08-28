f4f903b9cf3fbbfa95ef740a10001ca2c524d23b
tonychen-daylight <134320416+tonychen-daylight@users.noreply.github.com>
2024-08-27 22:25:27 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ st.sidebar.header('Trip History')
 container = st.sidebar.container(border=True)
 container.date_input("Start Date", datetime.date(2024, 6, 1))
 container.date_input("End Date", datetime.datetime.now())
+container.button("Pull Strip", type="primary")
 
 # Sample dataset
 data = [
