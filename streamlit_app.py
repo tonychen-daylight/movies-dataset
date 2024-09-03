fc24d37fc304155ffe221bce7f42b98fe5c91365
tonychen-daylight <134320416+tonychen-daylight@users.noreply.github.com>
2024-09-02 21:04:26 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,7 +68,7 @@ def data_cleanup():
         item["arrival"] = pd.to_datetime(item["arrival"], errors="coerce")
 
         # Add 'WeekDay' column
-        item["WeekDay"] = pd.to_datetime(item["dispatch"], errors="coerce").month
+        item["WeekDay"] = pd.to_datetime(item["dispatch"], errors="coerce").day
         df_copy.append(item)
         
     return df_copy
