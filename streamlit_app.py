90e222c51e431616c8d2d5a7001e9b64a039cb2d
tonychen-daylight <134320416+tonychen-daylight@users.noreply.github.com>
2024-09-02 18:03:16 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,6 +64,7 @@ def data_cleanup():
     df_copy = []
     for item in st.session_state.trip_history:
         df_copy.append(item)
+        print(item)
     
     
     # Convert 'dispatch' and 'arrival' to datetime
