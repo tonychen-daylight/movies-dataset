6a2abd19ff8c2eaa49a6f98b2ba51e28801daca9
tonychen-daylight <134320416+tonychen-daylight@users.noreply.github.com>
2024-09-03 14:53:47 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import pulp
 import datetime
 import requests
 import numpy as np
+st.set_page_config(page_title='Trip Optimization',page_icon=':rocket:', layout='wide', initial_sizebar_state='expanded')
 
 # Trip History
 st.logo("data/dylt-logo-header.png",link="https://www.dylt.com/",
