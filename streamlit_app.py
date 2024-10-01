99bd5129b593721fc003e16c1a122e878aff807c
tonychen-daylight <134320416+tonychen-daylight@users.noreply.github.com>
2024-10-01 10:26:22 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -584,7 +584,7 @@ if container2.button("Show Summary", type="secondary"):
     {'Loop Name': "EWR-DAL-ONT-Tuesday-Thursday-Saturday", 'Count Of Weeks': '1', 'Week Number': '36', 'Avg Loop Duration': 5.6},
     {'Loop Name': "EWR-DAL-ONT-Tuesday-Thursday-Friday", 'Count Of Weeks': '1', 'Week Number': '36', 'Avg Loop Duration': 5.6},
     {'Loop Name': "EWR-DAL-ONT-Tuesday-Friday-Sunday", 'Count Of Weeks': '2', 'Week Number': '36', 'Avg Loop Duration': 5.6},
-    {'Loop Name': "", 'Count Of Weeks': '', 'Week Number': '35', 'Avg Loop Duration': 5.7}
+    {'Loop Name': "", 'Count Of Weeks': '', 'Week Number': '35', 'Avg Loop Duration': 5.7},
     {'Loop Name': "EWR-DAL-ONT-Wednesday-Friday-Sunday", 'Count Of Weeks': '2', 'Week Number': '36', 'Avg Loop Duration': 5.6},
     {'Loop Name': "", 'Count Of Weeks': '', 'Week Number': '35', 'Avg Loop Duration': 5.7},
     {'Loop Name': "DAL-ONT-EWR-Thursday-Saturday-Tuesday", 'Count Of Weeks': '1', 'Week Number': '36', 'Avg Loop Duration': 5.6},
