8f5fc6173c449356d7308d0824ccf98d5eeb7a7c
tonychen-daylight <134320416+tonychen-daylight@users.noreply.github.com>
2024-10-01 16:17:24 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -582,7 +582,7 @@ if container2.button("Show Summary", type="secondary"):
     new_item = {
       "Loop Name": item["Loop_Name"],
       "Counts Of Week": 2,
-      "Week Number" =item["WeekNumber"]
+      "Week Number": item["WeekNumber"]
     }
     loopList_copy.append(new_item)
 
